e8b7073feb668db086ea3d3237f43555777666e2
Melissa <MelissaCode01@outlook.com>
2022-09-21 16:03:55 -0400

    initial commit
    Leetcode 1480 commit

---
+++ javascript/leetcode_1480_running_sum_1d_array.js
@@ -0,0 +1,64 @@
+/*
+1480. Running Sum of 1d Array
+
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+Return the running sum of nums.
+
+Example 1:
+    Input: nums = [1,2,3,4]
+    Output: [1,3,6,10]
+    Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+Example 2:
+    Input: nums = [1,1,1,1,1]
+    Output: [1,2,3,4,5]
+    Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+Example 3:
+    Input: nums = [3,1,2,10,1]
+    Output: [3,4,6,16,17]
+
+Constraints:
+1 <= nums.length <= 1000
+-10^6 <= nums[i] <= 10^6
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+Input: nums1 = [1,2,3,4]
+// Output: [1,3,6,10]
+
+Input: nums2 = [1,1,1,1,1]
+// Output: [1,2,3,4,5]
+
+Input: nums3 = [3,1,2,10,1]
+// Output: [3,4,6,16,17]
+
+    /* pseudocode
+
+    Goal: calculate index pairs in nums array then store the calculation in a new array
+
+    1. create a new array
+    2. iterate through nums array to find position value
+    3. create a variable to store sum of index pairs -- this will be a rolling (changing) value
+    4. push new variable sum into new array
+    5. return new array
+
+    */
+
+var runningSum = function(nums) {
+
+    let newArray = [];
+    let indexSum = 0; // needs to be outside of for loop scope
+
+    for(let i = 0; i <= nums.length - 1; i++){
+
+        let positionValue = nums[i];
+        indexSum += positionValue; 
+        newArray.push(indexSum);
+    }
+    return newArray;
+}
+console.log(runningSum(nums1))
+console.log(runningSum(nums2))
+console.log(runningSum(nums3))
