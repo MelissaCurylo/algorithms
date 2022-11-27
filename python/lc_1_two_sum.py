edaf8d17214b9b68723d586f130cb629cdf3a7fe
Melissa <MelissaCode01@outlook.com>
2022-11-27 04:26:23 -0500

    lc_1_two_sum
    lc_1_two_sum: O(n) solution improvement

--- python/lc_1_two_sum.py
+++ python/lc_1_two_sum.py
@@ -40,6 +40,77 @@ target2 = 6
 nums3 = [3,3] 
 target3 = 6
 
+# class Solution:
+#     def twoSum(self, nums: list[int], target: int) -> list[int]:
+def twoSum(nums: list[int], target: int) -> list[int]:
+    
+    newMap = {} # val : index
+
+    for numOne, numTwo in enumerate(nums):
+        current_value = target - numTwo
+        if current_value in newMap:
+            return [newMap[current_value], numOne]
+        newMap[numTwo] = numOne
+
+    return
+
+print(twoSum(nums1, target1))
+print(twoSum(nums2, target2))
+print(twoSum(nums3, target3))
+
+
+
+
+# Time Complexity: O(n) 
+    # Linear time due to n being elements in input array receiving the same number of operations on each element. 
+    # Only one for loop with one pass through needed to traverse the array. 
+
+# Space Complexity: O(n)
+    # Using memory by 
+
+"""
+Pseudocode/Solution:
+
+- Goal: Find target value with only two valid index positions using Hash Map data structure. 
+
+- Using Hash Map:
+    1. Create three variables:
+        - Empty hash map
+        - current_value
+    2. Iterate through list and locate the complement that equates target.
+        - current_value + x = target
+    
+
+    Notes:
+    - Math -> current_value + x = target
+        - x = target - current_value
+    - Map value to index. 
+    - Cannot use the same index twice.
+    - Incorporating enumerate() 
+        - enumerate() converts data object and returns and object via adding iterating counter value to object. 
+        - removes the need to instantiate i = 0.
+        - enumerate starts at zero by default.
+    
+
+
+
+    Hash Map View nums1
+    value : index
+    -------------
+    2   :  0
+    7   :  1
+    11  :  2
+    15  :  3
+
+
+    Hash Map View nums2
+    value : index
+    -------------
+    3   :  0
+    2   :  1
+    4   :  2
+    
+"""
 
 # Brute Force Solution 1
     # Time Complexity: O(n^2)
@@ -64,7 +135,9 @@ print(bruteForce(nums1, target1))
 def bruteForce2(nums, target):
     for numOne in range(len(nums)):
         for numTwo in range(numOne + 1, len(nums)):
-            if numTwo == target - numOne:
+            if nums[numTwo] == target - nums[numOne]:
                 return [numOne, numTwo]
+                
 
 print(bruteForce2(nums1,target1))
+print(bruteForce2(nums2,target2))
